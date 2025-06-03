cba957479ab67c5b85f0677c883e24f530dd7103
Taras Zherebetskyy <zhere97@gmail.com>
2025-06-02 21:00:17 -0400

    fmt

--- infrastructure/bin/app.ts
+++ infrastructure/bin/app.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
+import * as cdk from 'aws-cdk-lib';
 
-import { WebsiteStack } from "../lib/stacks/s3stack";
+import { WebsiteStack } from '../lib/stacks/s3stack';
 const app = new cdk.App();
-export const portfolio = "portfolio"
+export const portfolio = 'portfolio';
 
 new WebsiteStack(app, portfolio, {});
-
--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -3,9 +3,9 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { WebsiteStack } from '../lib/stacks/s3stack';
 import { portfolio } from '../bin/app';
 
-test("Snapshot Test", () => {
-    const app = new cdk.App();
-    const websiteStack = new WebsiteStack(app, portfolio);
-    const template = Template.fromStack(websiteStack);
-    expect(template).toMatchSnapshot();
+test('Snapshot Test', () => {
+  const app = new cdk.App();
+  const websiteStack = new WebsiteStack(app, portfolio);
+  const template = Template.fromStack(websiteStack);
+  expect(template).toMatchSnapshot();
 });
