299e117155d531023a95dbafc6c605ca741f8ef2
Taras Zherebetskyy <35704223+TarasZhere@users.noreply.github.com>
2025-06-05 23:04:42 -0400

    Update Prettier's settings (#7)

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,11 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { WebsiteStack } from '../lib/stacks/s3stack';
-import { portfolio } from '../bin/app';
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { WebsiteStack } from '../lib/stacks/s3stack'
+import { portfolio } from '../bin/app'
 
 test('Snapshot Test', () => {
-  const app = new cdk.App();
-  const websiteStack = new WebsiteStack(app, portfolio);
-  const template = Template.fromStack(websiteStack);
-  expect(template).toMatchSnapshot();
-});
+  const app = new cdk.App()
+  const websiteStack = new WebsiteStack(app, portfolio)
+  const template = Template.fromStack(websiteStack)
+  expect(template).toMatchSnapshot()
+})
