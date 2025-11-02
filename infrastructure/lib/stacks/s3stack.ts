e49ece14561b2636c1e4b18f7172c3ff08d0418f
Taras Zherebetskyy <35704223+TarasZhere@users.noreply.github.com>
2025-11-01 21:40:00 -0400

    Update domain name (#10)

--- infrastructure/lib/stacks/s3stack.ts
+++ infrastructure/lib/stacks/s3stack.ts
@@ -2,6 +2,18 @@ import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib'
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3'
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs'
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53'
+import {
+  Certificate,
+  CertificateValidation,
+} from 'aws-cdk-lib/aws-certificatemanager'
+import {
+  Distribution,
+  OriginAccessIdentity,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront'
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins'
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets'
 
 interface WebsiteProps extends StackProps {}
 
@@ -9,20 +21,50 @@ export class WebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: WebsiteProps) {
     super(scope, id, props)
     const self: Construct = this
+    const domainName = 'taras.click'
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId: 'Z038349417XTGZS79FSNC',
+      zoneName: domainName,
+    })
+
+    const certificate = new Certificate(this, 'MySiteCertificate', {
+      domainName,
+      subjectAlternativeNames: [`www.${domainName}`],
+      validation: CertificateValidation.fromDns(hostedZone),
+    })
 
     const s3Bucket = new Bucket(self, `${id}-bucket`, {
-      websiteIndexDocument: 'index.html',
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-      publicReadAccess: true,
-      bucketName: `${id}-bucket-us-east-2`,
-      blockPublicAccess: new BlockPublicAccess({
-        blockPublicAcls: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false,
-      }),
-      versioned: true,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    })
+
+    const oai = new OriginAccessIdentity(this, 'MySiteOAI')
+    s3Bucket.grantRead(oai)
+
+    const distribution = new Distribution(this, 'MySiteDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(s3Bucket, {
+          originAccessIdentity: oai,
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: [domainName, `www.${domainName}`],
+      certificate: certificate,
+      defaultRootObject: 'index.html',
+    })
+
+    new ARecord(this, 'MySiteARecord', {
+      zone: hostedZone,
+      recordName: domainName,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    })
+
+    new ARecord(this, 'MySiteWwwARecord', {
+      zone: hostedZone,
+      recordName: `www.${domainName}`,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
     })
 
     new BucketDeployment(self, `${id}-deployment`, {
@@ -31,7 +73,7 @@ export class WebsiteStack extends Stack {
     })
 
     new CfnOutput(self, `${id}-uri-link`, {
-      value: s3Bucket.bucketWebsiteUrl,
+      value: `https://${domainName}`,
     })
   }
 }
