1619d29cc3c917b6d4bbb30d1b5a9850721b3247
Taras Zherebetskyy <35704223+TarasZhere@users.noreply.github.com>
2025-11-02 23:36:11 -0500

    Fix (#24)

--- infrastructure/lib/stacks/s3stack.ts
+++ infrastructure/lib/stacks/s3stack.ts
@@ -72,6 +72,7 @@ export class WebsiteStack extends Stack {
       destinationBucket: s3Bucket,
     })
 
+    // Website output link
     new CfnOutput(self, `${id}-uri-link`, {
       value: `https://${domainName}`,
     })
